0b084cf2e295ecd6bc076ca39da358b92c381b31
Sheikh <tvemperror00@gmail.com>
2025-04-21 01:15:32 +0300

    Finished code in Nasm source and debugged it

--- 3/main.s
+++ 3/main.s
@@ -3,7 +3,9 @@ bits 64
 section .data
     file_buffer times 256 db 0
     shift_buffer dq 0
-    descriptor dd "input", 0
+    filename db "input", 0
+    descriptor dd 0
+    error_msg db "Input Format error", 0
 
 section .text
 global _start
@@ -15,16 +17,51 @@ global _start
 ; return: rax = encoded string
 ceasar_cipher:
     mov r9, rcx
-    mov rcx, 0
+    xor rcx, rcx
     
     .loop:
     cmp rcx, rdx
     jge .exit
-    cmp byte [rbx + rcx], 0
-    je .loop
-    mov r12, [rbx + rcx]
+    movzx r12, byte [rbx + rcx]
+    cmp r12, 'a'
+    jl .isalpha_else
+    cmp r12, 'z'
+    jl .islower_condition
+    cmp r12, 'A'
+    jl .isalpha_else
+    cmp r12, 'Z'
+    jl .isupper_condition
+    jmp .loop
+
+    .islower_condition:
+    sub r12, 'a'
     add r12, r9
-    mov [rbx + rcx], r12
+    mov rax, r12
+    push rdx
+    xor rdx, rdx
+    mov r12, 26
+    div r12
+    add rdx, 'a'
+    mov byte [rbx + rcx], dl
+    pop rdx
+    jmp .loop_step
+
+    .isupper_condition:
+    sub r12, 'A'
+    add r12, r9
+    mov rax, r12
+    push rdx
+    xor rdx, rdx
+    mov r12, 26
+    div r12
+    add rdx, 'A'
+    mov byte [rbx + rcx], dl
+    pop rdx
+    jmp .loop_step
+
+    .isalpha_else:
+
+    .loop_step:
     inc rcx
     jmp .loop
 
@@ -33,23 +70,34 @@ ceasar_cipher:
     ret
 
 _start:
-    .read_size:
+    mov rax, 2
+    mov rdi, filename
+    mov rsi, 0
+    mov rdx, 0
+    syscall
+
+    cmp rax, 0
+    jle .file_format_error
+    mov [descriptor], eax
+
+    .read_key:
     mov rax, 0
-    mov rdi, descriptor
+    mov edi, [descriptor]
     mov rsi, shift_buffer
-    mov rdx, 8
+    mov rdx, 2
     syscall
     cmp rax, 0
-    jle .exit
-    cmp rax, 8
+    jle .file_format_error
+    cmp rax, 2
     jl .file_format_error
-    mov r8, [shift_buffer]
+    movzx r8, byte [shift_buffer]
+    sub r8, '0'
     mov rcx, 0
     jmp .process_loop
 
     .process_loop:
     mov rax, 0
-    mov rdi, descriptor
+    mov edi, [descriptor]
     mov rsi, file_buffer
     mov rdx, 256
     syscall
@@ -70,16 +118,17 @@ _start:
     jmp .process_loop
 
     .exit:
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 60 ; exit
+    xor rdi, rdi ; return code 0
     syscall
 
     .file_format_error:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, "IO error"
-    mov rdx, 256
+    mov rax, 1 ; write
+    mov rdi, 1 ; stdout
+    mov rsi, error_msg ; error message
+    mov rdx, 19 ; length of error message
     syscall
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 60 ; exit
+    xor rdi, rdi 
+    mov rdi, 1 ; exit code 1 (error)
     syscall
