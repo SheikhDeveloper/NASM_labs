cd2c0f60257673e83a69b3152adf2ffedbc538ec
Sheikh <tvemperror00@gmail.com>
2025-04-21 01:34:22 +0300

    Add comments to Nasm code and fix little remaining bug

--- 3/main.s
+++ 3/main.s
@@ -16,21 +16,21 @@ global _start
 ; rdx - string length
 ; return: rax = encoded string
 ceasar_cipher:
-    mov r9, rcx
-    xor rcx, rcx
+    mov r9, rcx ; save shift value
+    xor rcx, rcx ; counter = 0
     
     .loop:
-    cmp rcx, rdx
-    jge .exit
-    movzx r12, byte [rbx + rcx]
-    cmp r12, 'a'
-    jl .isalpha_else
+    cmp rcx, rdx 
+    jge .exit ; if counter >= string length, exit
+    movzx r12, byte [rbx + rcx] ; get char
+    cmp r12, 'a' 
+    jl .isalpha_else ; if char < 'a', skip
     cmp r12, 'z'
-    jl .islower_condition
+    jle .islower_condition ; if 'a' <= char <= 'z', then it is lower case
     cmp r12, 'A'
-    jl .isalpha_else
+    jl .isalpha_else ; if 'z' < char < 'A', skip
     cmp r12, 'Z'
-    jl .isupper_condition
+    jle .isupper_condition ; if 'A' <= char <= 'Z', then it is upper case
     jmp .loop
 
     .islower_condition:
@@ -41,8 +41,8 @@ ceasar_cipher:
     xor rdx, rdx
     mov r12, 26
     div r12
-    add rdx, 'a'
-    mov byte [rbx + rcx], dl
+    add rdx, 'a' ; rdx = (char - 'a' + shift) % 26 + 'a'
+    mov byte [rbx + rcx], dl ; mov char to string
     pop rdx
     jmp .loop_step
 
@@ -54,52 +54,52 @@ ceasar_cipher:
     xor rdx, rdx
     mov r12, 26
     div r12
-    add rdx, 'A'
-    mov byte [rbx + rcx], dl
+    add rdx, 'A' ; rdx = (char - 'A' + shift) % 26 + 'A'
+    mov byte [rbx + rcx], dl ; mov char to string
     pop rdx
     jmp .loop_step
 
     .isalpha_else:
 
     .loop_step:
-    inc rcx
+    inc rcx ; increment counter
     jmp .loop
 
     .exit:
-    mov rax, rbx
+    mov rax, rbx ; return encoded string
     ret
 
 _start:
-    mov rax, 2
+    mov rax, 2 ; open file filename
     mov rdi, filename
-    mov rsi, 0
-    mov rdx, 0
+    mov rsi, 0 ; read only
+    mov rdx, 0 ; no flags
     syscall
 
-    cmp rax, 0
+    cmp rax, 0 ; check if file opened successfully
     jle .file_format_error
-    mov [descriptor], eax
+    mov [descriptor], eax ; save file descriptor
 
     .read_key:
-    mov rax, 0
-    mov edi, [descriptor]
+    mov rax, 0 ; read from descriptor
+    mov edi, [descriptor] ; file descriptor
     mov rsi, shift_buffer
-    mov rdx, 2
+    mov rdx, 2 ; read 2 bytes
     syscall
-    cmp rax, 0
+    cmp rax, 0 ; check if read was successful
     jle .file_format_error
-    cmp rax, 2
+    cmp rax, 2 ; check if read 2 bytes
     jl .file_format_error
     movzx r8, byte [shift_buffer]
-    sub r8, '0'
+    sub r8, '0' ; key = int(string(shift_buffer))
     mov rcx, 0
     jmp .process_loop
 
     .process_loop:
-    mov rax, 0
+    mov rax, 0 ; read from descriptor
     mov edi, [descriptor]
     mov rsi, file_buffer
-    mov rdx, 256
+    mov rdx, 256 ; read 256(buffer size) bytes
     syscall
     cmp rax, 0
     jle .exit
@@ -110,10 +110,10 @@ _start:
     call ceasar_cipher
     mov r13, rax
     pop rcx
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, r13
-    mov rdx, rdx
+    mov rax, 1 ; write
+    mov rdi, 1 ; stdout
+    mov rsi, r13 ; encoded string
+    mov rdx, rdx ; length of encoded string
     syscall
     jmp .process_loop
 
